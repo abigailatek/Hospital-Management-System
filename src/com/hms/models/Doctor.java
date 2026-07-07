82f377845a312a855a810a37d3f4780eb1e03600
nuwtimothy <nuwtimothy@gmail.com>
2026-07-07 12:40:58 +0300

    Implement Doctor Management module

--- src/com/hms/models/Doctor.java
+++ src/com/hms/models/Doctor.java
@@ -2,19 +2,96 @@ package com.hms.models;
 
 public class Doctor {
 
-    private String name;
-    private String specialization;
+    private int doctorId;
+    private int userId;
 
-    public Doctor(String name, String specialization) {
-        this.name = name;
-        this.specialization = specialization;
+    private String firstName;
+    private String lastName;
+    private String specialization;
+    private String phone;
+    private String email;
+
+    public Doctor() {
     }
 
-    public String getName() {
-        return name;
+    public Doctor(int doctorId,
+                  int userId,
+                  String firstName,
+                  String lastName,
+                  String specialization,
+                  String phone,
+                  String email) {
+
+        this.doctorId = doctorId;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.specialization = specialization;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getSpecialization() {
         return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return doctorId + " | "
+                + firstName + " "
+                + lastName + " | "
+                + specialization;
     }
 }
